4b09474b6aa824ddb796d19aaa5271810159ccfd
Michael Stiles <mstiles01@icloud.com>
2019-07-16 10:53:31 -0500

    very easy problems

---
+++ javascript/funprac.js
@@ -0,0 +1,18 @@
+// ***** Very Easy**********
+
+// Create a function that takes two numbers as arguments and return their sum.
+
+// const addFunction = (num1, num2) => console.log(num1 + num2);
+// addFunction(2, 3)
+
+// Write a function that takes the base and height of a triangle and return its area.
+
+// const triArea = (base, height) => console.log(base * height / 2);
+// triArea(8,5)
+
+// **********Easy********
+
+// Write two functions:
+// 1. toArray(), which converts a number to an array of its digits.
+// 2. toNumber(), which converts an array of digits back to its number.
+// Examples
